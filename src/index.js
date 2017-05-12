299691cc41af5328588fe94218a1b8c76f122832
github username <github email address>
2017-05-12 10:40:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+//append image from url and caption to page
+// form handling
+// define inputs
 $(document).ready(function(){
-  // your code here!
+  let $photoList = $('#photo-list');
+  let $form = $('form#photo-form');
+  let $imageInput = $('#image-input');
+
+  let $captionInput = $('#caption-input');
+
+  $form.submit(function(e){
+    e.preventDefault()
+    $postContainer = document.createElement('div');
+    $postContainer.setAttribute('class','post-container');
+    $postImage = document.createElement('img');
+    $postImage.setAttribute('class','post-image');
+    $postImage.setAttribute('src',$imageInput.val());
+    $postCaption = document.createElement('div');
+    $postCaption.setAttribute('class','post-caption');
+    $postCaptionEntry = document.createElement('span')
+    $postCaptionEntry.setAttribute('class','post-caption-entry');
+    $postCaptionEntry.innerHTML = $captionInput.val()
+
+
+    $postCaption.append($postCaptionEntry);
+    $postContainer.append($postImage);
+    $postContainer.append($postCaption);
+    $photoList.append($postContainer);
+
+  })
 })
